e1d94db0b4b23a7ae95912e71139f0b9d00c8f18
2272799702lxd <m15871692767@163.com>
2018-09-04 17:07:28 +0800

    first add

---
+++ README.txt
@@ -0,0 +1,5 @@
+dubbo-admin配置:
+ https://blog.csdn.net/u013142781/article/details/50396621
+
+ZuulKeeper配置搭建:
+ https://blog.csdn.net/u013142781/article/details/50395650
---
+++ service-provider/src/main/java/com/dubbo/service/impl/TestServiceImpl.java
@@ -0,0 +1,11 @@
+package com.dubbo.service.impl;
+
+import com.dubbo.service.TestService;
+
+public class TestServiceImpl implements TestService {
+
+
+    public String getName() {
+        return "adonai-dubbo-frame";
+    }
+}
